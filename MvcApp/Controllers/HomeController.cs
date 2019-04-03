f1f93c5b1b76f650b9ca32d1f7e44ae5e88b466a
Zeydel <Seydel14>
2019-04-03 20:28:59 +0200

    Add project files.

---
+++ MvcApp/Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApp.Models;
+
+
+namespace MvcApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		//  
+		// GET: /Home/  
+		ServiceReference1.MyServiceClient ur = new ServiceReference1.MyServiceClient();
+		public ActionResult Index()
+		{
+			List<User> lstRecord = new List<User>();
+
+			var lst = ur.GetAllUser();
+
+			foreach (var item in lst)
+			{
+				User usr = new User();
+				usr.Id = item.Id;
+				usr.Name = item.Name;
+				usr.Email = item.Email;
+				lstRecord.Add(usr);
+
+			}
+
+
+			return View(lstRecord);
+		}
+
+
+		public ActionResult Add()
+		{
+
+			return View();
+		}
+		[HttpPost]
+		public ActionResult Add(User mdl)
+		{
+
+			User usr = new User();
+			usr.Name = mdl.Name;
+			usr.Email = mdl.Email;
+			ur.AddUser(usr.Name, usr.Email);
+			return RedirectToAction("Index", "Home");
+
+		}
+		public ActionResult Delete(int id)
+		{
+			int retval = ur.DeleteUserById(id);
+			if (retval > 0)
+			{
+				return RedirectToAction("Index", "Home");
+			}
+
+			return View();
+		}
+
+		public ActionResult Edit(int id)
+		{
+			var lst = ur.GetAllUserById(id);
+			User usr = new User();
+			usr.Id = lst.Id;
+			usr.Name = lst.Name;
+			usr.Email = lst.Email;
+			return View(usr);
+
+		}
+		[HttpPost]
+		public ActionResult Edit(User mdl)
+		{
+			User usr = new User();
+			usr.Id = mdl.Id;
+			usr.Name = mdl.Name;
+			usr.Email = mdl.Email;
+
+
+			int Retval = ur.UpdateUser(usr.Id, usr.Name, usr.Email);
+			if (Retval > 0)
+			{
+				return RedirectToAction("Index", "Home");
+			}
+			return View();
+		}
+	}
+}
---
+++ WcfServiceApp/IMyService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceApp
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IMyService" in both code and config file together.  
+	[ServiceContract]
+	public interface IMyService
+	{
+		[OperationContract]
+		List<UserDetail> GetAllUser();
+		[OperationContract]
+		int AddUser(string Name, string Email);
+		[OperationContract]
+		UserDetail GetAllUserById(int id);
+
+		[OperationContract]
+		int UpdateUser(int Id, string Name, string Email);
+
+		[OperationContract]
+		int DeleteUserById(int Id);
+	}
+
+
+	[DataContract]
+	public class UserDetails
+	{
+		[DataMember]
+		public int Id { get; set; }
+		[DataMember]
+		public string Name { get; set; }
+		[DataMember]
+		public string Email { get; set; }
+
+
+	}
+}
